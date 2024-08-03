5f6ef8f9cc70a759be74e66a5fc94539d06a7e89
bertolima <bertolilima27@gmail.com>
2024-08-03 11:43:52 -0300

    test

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from fileManager import fileManager
 from Registro import Registro
 import queue
-from heapq import *
+from Heap import Heap
 
 if __name__ == "__main__":
-    heap = []
-    heappush(heap, Registro(5))
-    heappush(heap, Registro(2,1))
-    heappush(heap, Registro(10))
+    heap = Heap(3)
+    heap.push(Registro(5))
+    new_registro = Registro(2)
+    new_registro.setFlag()
+    heap.push(new_registro)
+    heap.push(Registro(10))
 
     while len (heap) > 0: 
-        print(heap[0].value)
-        heappop(heap)
+        print(heap.first().value)
+        heap.pop()
