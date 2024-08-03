56766abdee67616714c036994534d7286eb4d2a6
bertolima <bertolilima27@gmail.com>
2024-08-03 09:38:33 -0300

    files manager class

---
+++ fileManager.py
@@ -0,0 +1,18 @@
+import os
+
+
+class fileManager:
+
+    @staticmethod
+    def read(filePath:str) -> tuple:
+
+        f = open(filePath, 'r')
+
+        mode = f.readline()[0]
+        specs = [int(x) for x in f.readline().split()]
+        numbers = [int(x) for x in f.readline().split()]  
+        f.close()
+
+        return mode, specs, numbers
+        
+
