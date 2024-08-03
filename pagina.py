e94cbeb864a9c4f19d2b7c3f029c7a2776ea68a2
bertolima <bertolilima27@gmail.com>
2024-08-03 09:39:15 -0300

    represents a page in memory

---
+++ pagina.py
@@ -0,0 +1,16 @@
+class pagina:
+    def __init__(self):
+        self.registros = []
+
+    def get(self, index:int) -> int:
+        if (self.registros[index] is not None):
+            return self.registros[index]
+        return -1;
+
+    def set(self, index:int, value:int):
+        if(len(self.registros) > index):
+            self.registros[index] = value
+
+    def add(self, value:int):
+        self.registros.append(value)
+        
