6543ca71334369df6e7a20a4306fa2da2c32a656
caiogmello <caiogomedemello@gmail.com>
2024-08-03 11:37:47 -0300

    teste das classes

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from fileManager import fileManager
 from Registro import Registro
 import queue
 from heapq import *
+from Heap import Heap
 
 if __name__ == "__main__":
-    heap = []
-    heappush(heap, Registro(5))
-    heappush(heap, Registro(2,1))
-    heappush(heap, Registro(10))
+    heap = Heap(3)
 
-    while len (heap) > 0: 
-        print(heap[0].value)
-        heappop(heap)
+    heap.push(Registro(1))
+    heap.push(Registro(10))
+    heap.push(Registro(4))
+
+    heap.flagAll()
+
+    print(heap.isFullFlagged())
+
+    heap.unflagAll()
+
+    print(heap.isFullFlagged())
+
