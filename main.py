005ff65d89a12493559d961485500d52ceb29c0c
Caio Viana <caiohebertv@gmail.com>
2024-08-05 01:12:37 -0300

    add testes melhores

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     # mode, specs, values = fileManager.read('example1')
 
     # exemplo polifasica
-    mode, specs, values = fileManager.read('example1-poli')
+    mode, specs, values = fileManager.read('hard_example')
 
     # exemplo cascata
-    # mode, specs, values = fileManager.read('example1-casca')
+    # mode, specs, values = fileManager.read('hard_example2')
 
     ordenador = ordenador()
     ordenador.ordenar(mode, specs[0], specs[1], specs[2], specs[3], values)
