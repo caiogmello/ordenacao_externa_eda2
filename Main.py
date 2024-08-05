cb49de0679794db7e79a8b713355ca054b205733
caiogmello <caiogomedemello@gmail.com>
2024-08-05 20:03:45 -0300

    colocando pra receber entrada via segundo argumento no terminal

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 from FileManager import fileManager
 from Registro import Registro
-from Ordenador import ordenador
+from Ordenador import Ordenador
+import sys
+
 
 if __name__ == "__main__":
     
@@ -8,10 +10,13 @@ if __name__ == "__main__":
     # mode, specs, values = fileManager.read('example1')
 
     # exemplo polifasica
-    mode, specs, values = fileManager.read('hard_example')
+    if(len(sys.argv) > 1):
+        mode, specs, values = fileManager.read(sys.argv[1])
+    else:
+        mode, specs, values = fileManager.read('hard_example')
 
     # exemplo cascata
     # mode, specs, values = fileManager.read('hard_example2')
 
-    ordenador = ordenador()
+    ordenador = Ordenador()
     ordenador.ordenar(mode, specs[0], specs[1], specs[2], specs[3], values)
