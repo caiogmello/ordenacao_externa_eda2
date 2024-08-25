f189edcfd5c145baf6d7101179e0b9ff678b5923
caiogmello <caiogomedemello@gmail.com>
2024-08-25 09:40:27 -0300

    refatorando

--- GeradorAlphasEBetas.py
+++ GeradorAlphasEBetas.py
@@ -5,19 +5,19 @@ g = GeradorExperimentos(3, 8, 200000)
 
 
 dctt_alpha_B = g.getAlphaDict(50000, "B", 5)
-saveJson(dctt_alpha_B, "alphaMultiCaminhos.json", "alphas")
+JsonManager.saveJson(dctt_alpha_B, "alphaMultiCaminhos.json", "alphas")
 
 dctt_beta_B = g.getBetaDict(100000, "B", 10)
-saveJson(dctt_beta_B, "betaMultiCaminhos.json", "betas")
+JsonManager.saveJson(dctt_beta_B, "betaMultiCaminhos.json", "betas")
 
 dctt_alpha_C = g.getAlphaDict(50000, "C", 5)
-saveJson(dctt_alpha_C, "alphaCascata.json", "alphas")
+JsonManager.saveJson(dctt_alpha_C, "alphaCascata.json", "alphas")
 
 dctt_beta_C = g.getBetaDict(100000, "C", 10)
-saveJson(dctt_beta_C, "betaCascata.json", "betas")
+JsonManager.saveJson(dctt_beta_C, "betaCascata.json", "betas")
 
 dctt_alpha_P = g.getAlphaDict(50000, "P", 5)
-saveJson(dctt_alpha_P, "alphaPolifasica.json", "alphas")
+JsonManager.saveJson(dctt_alpha_P, "alphaPolifasica.json", "alphas")
 
 dctt_beta_P = g.getBetaDict(100000, "P", 10)
-saveJson(dctt_beta_P, "betaPolifasica.json", "betas")
+JsonManager.saveJson(dctt_beta_P, "betaPolifasica.json", "betas")
--- Ordenador.py
+++ Ordenador.py
@@ -2,33 +2,34 @@ from collections import deque
 from Pagina import Pagina
 from Heap import Heap
 from Registro import Registro
+import numpy as np
 
 class Ordenador:
     def __init__(self):
         self.paginas:deque[Pagina] = deque()     
-        self.nRegistros = 0
-        self.betas = []
-        self.alpha = -1.0
-        self.paginaFinal = None
+        self.nRegistros: int = 0
+        self.betas: list[float] = []
+        self.alpha_r: float = -1.0
+        self.paginaFinal: deque[Registro] = None
 
     def ordenar(self, method:str,
                  m:int, k:int, r:int, n:int,
-                   registros:list[int], teste: bool = False, to_print: bool = True) -> None:
-        self.criarPaginas(k)
-        self.betas.clear()
-        if (method == "B"): 
-            limitePaginas = k//2
-        else: 
-            limitePaginas = k-1
+                   registros:list[int],
+                     experimental: bool = False, to_print: bool = True) -> None:
         
-        if not teste:
-            self.gerarSequencais(registros, m, r, limitePaginas)
-        if teste:
-            self.paginas = self.gerarRSequencias(n, r, k, method)
+        self.restart()
+        self.paginas = self.criarPaginas(k)
+
+        limite_de_paginas: int = self.setLimiteDePaginas(k, method)
+        
+        if not experimental:
+            self.gerarSequencias(registros, m, r, limite_de_paginas)
+        else:
+            self.paginas = self.gerarRSequenciasAleatorias(n, r, k, method)
         
         match method:
             case "B":
-                self.balanceadaMultiCaminhos(m, to_print, )
+                self.balanceadaMultiCaminhos(m, to_print)
             case "P":
                 self.polifasica(m, to_print)
             case "C":
@@ -36,32 +37,34 @@ class Ordenador:
             case _:
                 self.balanceadaMultiCaminhos(m, to_print)
 
+
+
+    def restart(self) -> None:
         self.paginas.clear()
         self.nRegistros = 0
-
+        self.betas.clear()
+        self.alpha_r = -1.0
+        self.paginaFinal = None
 
     def criarPaginas(self, k:int) -> None:
-        for i in range(k):
-            self.paginas.append(Pagina(i))
-    
-    def gerarRSequencias(self, n, r, k, metodo):
-        import numpy as np
-                
-        limite_de_paginas = k-1
-        if metodo == "B":
-            limite_de_paginas = k//2
-            
         paginas = deque()
         for i in range(k):
             paginas.append(Pagina(i))
+
+        return paginas
+    
+    def gerarRSequenciasAleatorias(self, n, r, k, metodo):
+                
+        limite_de_paginas: int = self.setLimiteDePaginas(k, metodo)
+            
+        paginas: deque[Pagina] = self.criarPaginas(k)
         
         sequencia = deque()
-        arquivos_possiveis = list(np.random.permutation(n))
-
+        arquivos_possiveis: list[int] = self.permutacao(n)
         
-        page_counter = 0
-        seq_counter = 0
-        n_registros = 0
+        page_counter: int = 0
+        seq_counter: int = 0
+        n_registros: int = 0
 
 
         if(r % limite_de_paginas == 0):
@@ -75,7 +78,6 @@ class Ordenador:
             if(len(sequencia)==0):
                 sequencia.append(Registro(arquivos_possiveis[n_registros]))
                 n_registros+=1
-                i+=1
                 continue
 
             if(sequencia[-1].value < Registro(arquivos_possiveis[n_registros]).value):
@@ -90,20 +92,22 @@ class Ordenador:
 
                      
         self.nRegistros = n_registros
-        # print(arquivos_possiveis[:n_registros])
         
         return paginas
 
 
-    def gerarSequencais(self, registros:list[int],
+    def permutacao(self, n:int) -> list[int]:
+        return list(np.random.permutation(n))
+
+    def gerarSequencias(self, registros:list[int],
                          m:int, r:int,
                            limitePaginas:int) -> None:
         heap = Heap()
         sequence:deque[Registro] = deque()
         sequencias = []
-        pageCounter = 0
-        sequenceCounter = 0
-        count = 0
+        pageCounter: int = 0
+        sequenceCounter: int = 0
+        count: int = 0
 
         for elem in registros:
             new_registro = Registro(elem)
@@ -129,7 +133,7 @@ class Ordenador:
                 sequenceCounter+=1
 
                 if(sequenceCounter == r): 
-                    break;
+                    break
 
                 sequence = deque()
             
@@ -156,13 +160,13 @@ class Ordenador:
 
 
     def balanceadaMultiCaminhos(self, m:int, to_print:bool=True) -> None:
-        count = 0
-        writes = 0.0
+        count: int = 0
+        writes: float = 0.0
         while(not self.isOrdered()):
             filled = [x for x in self.paginas if (not x.isEmpty())]
             notFilled = [x for x in self.paginas if (x.isEmpty())]
 
-            self.imprimir_resultados(filled, count, m, to_print)
+            self.computarResultados(filled, count, m, to_print)
 
             # roda o loop até que os primeiro arquivo de filled esteja vazio (terminou a intercalação da fase)
             while not filled[0].isEmpty():
@@ -175,12 +179,16 @@ class Ordenador:
             count+=1
 
         filled = [x for x in self.paginas if (not x.isEmpty())]
-        self.imprimir_resultados(filled, count, m, to_print)
+        self.computarResultados(filled, count, m, to_print)
         self.paginaFinal = filled
-        self.alpha = self.calcular_alpha(writes)
+        self.alpha_r = self.alpha(writes)
         if to_print:
-            print("Final", f"{self.alpha:.2f}")
+            print("Final", f"{self.alpha_r:.2f}")
             
+    def setLimiteDePaginas(self, k:int, metodo:str) -> int:
+        if (metodo == "B"): 
+            return k//2
+        return k-1
         
     def intercalar(self, filled:deque[Pagina], target:Pagina) -> float:
         nWrites = 0.0
@@ -208,9 +216,9 @@ class Ordenador:
         while(not self.isOrdered()):
             filled = [x for x in self.paginas if (not x.isEmpty())]
             notFilled = [x for x in self.paginas if (x.isEmpty())]
-            smallest_subsequence_page = self.get_smallest_page(filled)
+            smallest_subsequence_page = self.getSmallestPage(filled)
 
-            self.imprimir_resultados(filled, count, m, to_print)
+            self.computarResultados(filled, count, m, to_print)
 
             writes += self.intercalarPolifasica(smallest_subsequence_page, filled, notFilled[0])
             [x.active() for x in filled if x.isBlocked()]
@@ -219,11 +227,11 @@ class Ordenador:
 
         
         filled = [x for x in self.paginas if (not x.isEmpty())]
-        self.imprimir_resultados(filled, count, m, to_print)
+        self.computarResultados(filled, count, m, to_print)
         self.paginaFinal = filled
-        self.alpha = self.calcular_alpha(writes)
+        self.alpha_r = self.alpha(writes)
         if to_print:
-            print("Final", f"{self.alpha:.2f}")
+            print("Final", f"{self.alpha_r:.2f}")
 
     def intercalarPolifasica(self, smallest_page:Pagina, filled:deque[Pagina], target:Pagina) -> float:
         nWrites = 0.0
@@ -258,7 +266,7 @@ class Ordenador:
             notFilled = [x for x in self.paginas if (x.isEmpty())]
             target = notFilled[0]
 
-            self.imprimir_resultados(filled, count, m, to_print)
+            self.computarResultados(filled, count, m, to_print)
 
             maiorPagina: Pagina = max(self.paginas, key=lambda x: len(x))
 
@@ -276,12 +284,12 @@ class Ordenador:
 
         filled = [x for x in self.paginas if (not x.isEmpty())]
         self.paginaFinal = filled
-        self.imprimir_resultados(filled, count, m, to_print)
-        self.alpha = self.calcular_alpha(writes)
+        self.computarResultados(filled, count, m, to_print)
+        self.alpha_r = self.alpha(writes)
         if to_print:
-            print("Final", f"{self.alpha:.2f}")
+            print("Final", f"{self.alpha_r:.2f}")
 
-    def get_smallest_page(self, filled:deque[Pagina]):
+    def getSmallestPage(self, filled:deque[Pagina]):
         smallest = filled[0]
         for i in range(1, len(filled)):
             if(filled[i].getSequencesCount() < smallest.getSequencesCount()):
@@ -307,7 +315,7 @@ class Ordenador:
 
     #So organiza o print e calcula os resultados
 
-    def calcular_beta(self, filled:list[Pagina], m):
+    def beta(self, filled:list[Pagina], m):
         sequencesCount = 0
         for x in filled:
             sequencesCount+= x.getSequencesCount()
@@ -315,14 +323,14 @@ class Ordenador:
         b = (1/(m*sequencesCount))*self.nRegistros
         return b
     
-    def calcular_alpha(self, writes):
+    def alpha(self, writes):
         return round(writes/self.nRegistros,4)
         
         
-    def imprimir_resultados(self, filled:list[Pagina], count:int, m:int, to_print:bool=True):
+    def computarResultados(self, filled:list[Pagina], count:int, m:int, to_print:bool=True):
         if to_print:
             print("Fase", count, end=" ")
-        b = self.calcular_beta(filled, m)
+        b = self.beta(filled, m)
         self.betas += [round(b,4)]
 
         if to_print:
